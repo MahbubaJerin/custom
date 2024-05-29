f8c8205078e3b8311d3e39d82d5cb809f223567c
Sadnam Sakib Apurbo <sadnamsakib@iut-dhaka.edu>
2024-05-29 21:28:18 +0600

    liking post changes icon color

--- src/components/homepage/ViewPost.vue
+++ src/components/homepage/ViewPost.vue
@@ -21,9 +21,15 @@ fetchUserName();
 var posts = ref([]);
 const selectedPost = ref(null);
 const newComment = ref('');
+const liked = ref('');
 const fetchPosts = async () => {
   const querySnapshot = await getDocs(postRef);
   querySnapshot.forEach((doc) => {
+    if (doc.data().likes.includes(uid)) {
+      liked.value = true;
+    } else {
+      liked.value = false;
+    }
     posts.value.push(
       {
         id: doc.id,
@@ -35,7 +41,8 @@ const fetchPosts = async () => {
         commentsCount: doc.data().commentsCount,
         likes: doc.data().likes,
         comments: doc.data().comments,
-        createdAt: doc.data().createdAt
+        createdAt: doc.data().createdAt,
+        liked: liked.value
       }
     );
   });
@@ -55,6 +62,8 @@ const dislike = async (postId) => {
     for (let i = 0; i < posts.value.length; i++) {
       if (posts.value[i].id === postId) {
         posts.value[i].likesCount = postData.likesCount - 1;
+        posts.value[i].likes = postData.likes.filter((id) => id !== uid);
+        posts.value[i].liked = false;
       }
     }
     await updateDoc(docRef, {
@@ -81,6 +90,8 @@ const like = async (postId) => {
     for (let i = 0; i < posts.value.length; i++) {
       if (posts.value[i].id === postId) {
         posts.value[i].likesCount = postData.likesCount + 1;
+        posts.value[i].likes.push(uid);
+        posts.value[i].liked = true;
       }
     }
     await updateDoc(docRef, {
@@ -175,7 +186,7 @@ const addComment = async () => {
                 </div>
                 <div class="d-flex align-items-center ms-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="like-icon"
-                    @click="like(post.id)">
+                    :style="{ fill: post.liked ? '#FFD700' : '#808080' }" @click="like(post.id)">
                     <path
                       d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z" />
                   </svg>
@@ -201,7 +212,7 @@ const addComment = async () => {
   width: 20px;
   height: 20px;
   cursor: pointer;
-  fill: #FFD700;
+
   /* Color for like button */
 }
 
