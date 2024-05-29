5ee60dd93b2976b4c06277dd1eda50c422162126
Sadnam Sakib Apurbo <sadnamsakib@iut-dhaka.edu>
2024-05-29 21:39:38 +0600

    more bug fix

--- src/components/profile/profilePosts.vue
+++ src/components/profile/profilePosts.vue
@@ -8,6 +8,7 @@ const auth = getAuth();
 const myid = auth.currentUser.uid;
 const uid = window.location.pathname.split('/')[2];
 const userName = ref('');
+
 const fetchUserName = async () => {
     const docRef = doc(userRef, uid);
     const docSnap = await getDoc(docRef);
@@ -30,6 +31,10 @@ const fetchPosts = async () => {
     const querySnapshot = await getDocs(postRef);
     querySnapshot.forEach((doc) => {
         if (doc.data().uid === uid) {
+            var liked = false;
+            if (doc.data().likes.includes(myid)) {
+                liked = true;
+            }
             posts.value.push(
                 {
                     id: doc.id,
@@ -40,7 +45,8 @@ const fetchPosts = async () => {
                     commentsCount: doc.data().commentsCount,
                     likes: doc.data().likes,
                     comments: doc.data().comments,
-                    createdAt: doc.data().createdAt
+                    createdAt: doc.data().createdAt,
+                    liked: liked
                 }
             );
         }
@@ -59,11 +65,13 @@ const dislike = async (postId) => {
         for (let i = 0; i < posts.value.length; i++) {
             if (posts.value[i].id === postId) {
                 posts.value[i].likesCount = postData.likesCount - 1;
+                posts.value[i].likes = postData.likes.filter((id) => id !== myid);
+                posts.value[i].liked = false;
             }
         }
         await updateDoc(docRef, {
             likesCount: postData.likesCount - 1,
-            likes: postData.likes.filter((id) => id !== uid)
+            likes: postData.likes.filter((id) => id !== myid)
         });
     } else {
         console.log("No such document!");
@@ -85,11 +93,13 @@ const like = async (postId) => {
         for (let i = 0; i < posts.value.length; i++) {
             if (posts.value[i].id === postId) {
                 posts.value[i].likesCount = postData.likesCount + 1;
+                posts.value[i].likes = [...postData.likes, myid];
+                posts.value[i].liked = true;
             }
         }
         await updateDoc(docRef, {
             likesCount: postData.likesCount + 1,
-            likes: [...postData.likes, uid]
+            likes: [...postData.likes, myid]
         });
     } else {
         console.log("No such document!");
@@ -175,7 +185,7 @@ const addComment = async () => {
                                 </div>
                                 <div class="d-flex align-items-center ms-3">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="thumbs-up-icon"
-                                        @click="like(post.id)">
+                                        :style="{ fill: post.liked ? '#FFD700' : '#808080' }" @click="like(post.id)">
                                         <path
                                             d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z" />
                                     </svg>
@@ -202,7 +212,6 @@ const addComment = async () => {
     width: 20px;
     height: 20px;
     cursor: pointer;
-    fill: #FFD700;
 }
 
 .comment-icon {
